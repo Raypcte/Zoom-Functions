e30df7e64d2e59da559c4ea5d7d1c8b7b2091b85
Raypcte <rayanevalak@gmail.com>
2022-07-05 11:36:02 -0300

    questao1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,27 @@
-const data = require('../data/zoo_data');
+const data = require("../data/zoo_data");
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  console.log(ids);
+  if (ids.length === 0) {
+    return [];
+  }
+  if (ids.length === 1) {
+    const bicho = data.species.find((specie) => {
+      // find = encontrar um itém do Array
+      return ids[0] === specie.id;
+    });
+    return [bicho];
+  }
+
+  if (ids.length > 1) {
+    const bichos = [];
+    ids.forEach((id) => {
+      const bicho = data.species.find((specie) => id === specie.id);
+      bichos.push(bicho);
+    });
+    return bichos;
+  }
 }
 
+console.log(getSpeciesByIds());
 module.exports = getSpeciesByIds;
