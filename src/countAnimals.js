15788ae0f1de4499670e2b2f8891e092059dcaf7
Raypcte <rayanevalak@gmail.com>
2022-07-10 20:59:10 -0300

    questao 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const quantidade = {};
+  data.species.forEach((bicho) => {
+    quantidade[bicho.name] = bicho.residents.length;
+  });
+
+  if (animal === undefined) {
+    return quantidade;
+  }
+
+  let raça = quantidade[animal.specie];
+
+  if (animal.sex !== undefined) {
+    raça = data.species
+      .find((bicho) => bicho.name === animal.specie).residents
+      .filter((bicho) => bicho.sex === animal.sex).length;
+  }
+  return raça;
 }
 
 module.exports = countAnimals;
