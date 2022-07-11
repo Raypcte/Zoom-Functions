76c42289442aa095097d377ead4d0085168b6fe3
Raypcte <rayanevalak@gmail.com>
2022-07-11 03:22:35 -0300

    questao 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,33 @@
 const data = require('../data/zoo_data');
+// child: são pessoas menores de 18 anos;
+
+// adult: são pessoas com idade maior ou igual a 18 anos e menor que 50 anos;
+
+// senior: são pessoas com idade maior ou igual a 50 anos.
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const entradas = { child: 0, adult: 0, senior: 0 };
+
+  entrants.forEach((pessoa) => {
+    if (pessoa.age < 18) {
+      entradas.child += 1;
+    } else if (pessoa.age >= 18 && pessoa.age < 50) {
+      entradas.adult += 1;
+    } else {
+      entradas.senior += 1;
+    }
+  });
+
+  return entradas;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || entrants.length === 0 || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const pessoas = countEntrants(entrants);
+  const valorTotal = pessoas.adult * 49.99 + pessoas.child * 20.99 + pessoas.senior * 24.99;
+  return valorTotal;
 }
 
 module.exports = { calculateEntry, countEntrants };
