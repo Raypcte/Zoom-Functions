de855cbe8b5af2c22309b233041fe429b82443ee
Raypcte <rayanevalak@gmail.com>
2022-07-11 02:51:36 -0300

    questao 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const empregado = data.employees.find((funcionario) => funcionario.id === id);
+  const bichoId = empregado.responsibleFor[0];
+  let idade = 0;
+  let bichoMaisVelho;
+
+  data.species.find((bicho) => (
+    bicho.id === bichoId
+  )).residents.forEach((animal) => {
+    if (animal.age > idade) {
+      idade = animal.age;
+      bichoMaisVelho = animal;
+    }
+  });
+
+  return Object.values(bichoMaisVelho);
 }
 
 module.exports = getOldestFromFirstSpecies;
